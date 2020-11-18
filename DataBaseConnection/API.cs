4efcdc6bdcd1b16791ed35e1e18775bee8e3d03f
Alan Weik <alaan.m@hotmail.com>
2020-11-18 18:29:37 +0100

    Finisihed API class withour errors.

--- DataBaseConnection/API.cs
+++ DataBaseConnection/API.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataBaseConnection
 {
     class API
     {
+        public static List<Movie> GetMovieSlice(int a, int b)
+        {
+            using var ctx = new Context();
+            return ctx.Movies.OrderBy(m => m.Title).Skip(a).Take(b).ToList();
+        }
+        public static Customer GetCustomerByName(string name)
+        {
+            using var ctx = new Context();
+            return ctx.Customers.FirstOrDefault(c => c.Name.ToLower() == name.ToLower());
+        }
+        public static bool RegisterSale(Customer customer, Movie movie)
+        {
+            using var ctx = new Context();
+            try
+            {
+                ctx.Entry(customer).State = EntityState.Unchanged;
+                ctx.Entry(movie).State = EntityState.Unchanged;
+
+                ctx.Add(new Rental() { Date = DateTime.Now, Customer = customer, Movie = movie });
+                return ctx.SaveChanges() == 1;
+            }
+            catch (DbUpdateException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                System.Diagnostics.Debug.WriteLine(e.InnerException.Message);
+                return false;
+            }
+        }
     }
 }
