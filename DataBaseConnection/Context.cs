979d9dc2cd101bd6c3ecc08ba5cbfe8c5b5681a8
Alan Weik <alaan.m@hotmail.com>
2020-11-18 18:03:30 +0100

    Add project files.

---
+++ DataBaseConnection/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseConnection
+{
+    public class Context : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Rental> Sales { get; set; }
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            //.LogTo(s => System.Diagnostics.Debug.WriteLine(s))
+            .UseLazyLoadingProxies()
+            .UseSqlServer(
+            @"server")
+    }
+}
