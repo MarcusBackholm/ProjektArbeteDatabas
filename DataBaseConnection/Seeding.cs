83d900dba62e663ef07751308f6e629ff148671d
Alan Weik <alaan.m@hotmail.com>
2020-11-18 18:44:57 +0100

    Finished Seeding class with no errors.

--- DataBaseConnection/Seeding.cs
+++ DataBaseConnection/Seeding.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace DataBaseConnection
 {
     class Seeding
     {
+        static void Main()
+        {
+            using (var ctx = new Context())
+            {
+                ctx.RemoveRange(ctx.Sales);
+                ctx.RemoveRange(ctx.Movies);
+                ctx.RemoveRange(ctx.Customers);
+
+                ctx.AddRange(new List<Customer> {
+                    new Customer { Name = "Alan" },
+                    new Customer { Name = "Svante" },
+                    new Customer { Name = "Marcus" },
+                });
+
+                var movies = new List<Movie>();
+                var lines = File.ReadAllLines(@"..\..\..\SeedData\MovieGenre.csv");
+                for (int i = 0; i < 200; i++)
+                {
+                    var cells = lines[i].Split(',');
+
+                    var url = cells[5].Trim('"');
+
+                    try{ var test = new Uri(url); }
+                    catch (Exception) { continue; }
+
+                    movies.Add(new Movie { Title = cells[2], ImageURL = url });
+                }
+                ctx.AddRange(movies);
+
+                ctx.SaveChanges();
+            }
+        }
     }
 }
