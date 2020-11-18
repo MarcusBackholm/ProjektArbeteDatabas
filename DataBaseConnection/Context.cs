83f24379eda06f53972c7674f552d8d8fcaf3d3d
Alan Weik <alaan.m@hotmail.com>
2020-11-18 18:32:38 +0100

    FIxed error in Context by removing scope thingys.

--- DataBaseConnection/Context.cs
+++ DataBaseConnection/Context.cs
@@ -8,23 +8,22 @@ namespace DataBaseConnection
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Rental> Sales { get; set; }
-    }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder
-            //.LogTo(s => System.Diagnostics.Debug.WriteLine(s))
-            .UseLazyLoadingProxies()
-            .UseSqlServer(
-            @"server=.\SQLExpress;" +
-            @"database=SaleDataBase;" +
-            @"trusted_connection=true;" +
-            @"MultipleActiveResultSets=True"
-            );
-    }
-    
-    protected override void OnMOdelCreating(ModelBuilder modelBuilder)
-    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                //.LogTo(s => System.Diagnostics.Debug.WriteLine(s))
+                .UseLazyLoadingProxies()
+                .UseSqlServer(
+                @"server=.\SQLExpress;" +
+                @"database=SaleDataBase;" +
+                @"trusted_connection=true;" +
+                @"MultipleActiveResultSets=True"
+                );
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
     }
 }
